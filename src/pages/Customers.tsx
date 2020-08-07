bae2b297c82c8aa91920646719ad14780986811c
sumit <dhruvanwd@gmail.com>
2020-08-08 00:08:52 +0530

    removed comps

--- src/pages/Customers.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Customers() {
-  return (
-    <>
-      <h2>Customers</h2>
-    </>
-  );
-}
--- src/pages/Services.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Services() {
-  return (
-    <>
-      <h2>Services</h2>
-    </>
-  );
-}
